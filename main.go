aa62f0a327f565973c51ff1233002ce59aa95432
elis <huyixin5@163.com>
2021-06-15 19:47:28 +0800

    添加模板文件及配置json

--- main.go
+++ main.go
@@ -2,20 +2,17 @@ package main
 
 import (
 	"WT/entry"
-	util "WT/util"
+	"WT/util"
 )
 
-func errorPrint(err error)  {
-	if err!=nil {
+func errorPrint(err error) {
+	if err != nil {
 		print(err.Error())
 		return
 	}
 }
 
 func main() {
-
-	//s := "今日完成:\r\n1.日常巡检6台服务器6个信息系统(100%)1.5小时\r\n2.测试服务器完成管线中心OA系统信息化需求确认功能(100%)3小时\r\n3.编写管线中心OA系统项目预算模块(5%)3.5小时\r\n今日完成:\r\n1.日常巡检6台服务器6个信息系统(100%)1.5小时\r\n2.测试服务器完成管线中心OA系统信息化需求确认功能(100%)3小时\n3.编写管线中心OA系统项目预算模块(5%)3.5小时\r\n今日完成:"
-	//TextParsingRewrite(s)
 
 	deploy, err := util.ToDeploy()
 	errorPrint(err)
@@ -35,6 +32,5 @@ func main() {
 	// 写入发送内容模板
 	// 指定发送人
 	// 写入定时器
-
 
 }
