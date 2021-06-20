d9d1d94e2128e2a350dd8caf471b502fdd0ff628
elis <huyixin5@163.com>
2021-06-20 15:44:14 +0800

    添加简单错误处理,减少重复代码量

--- main.go
+++ main.go
@@ -5,29 +5,24 @@ import (
 	"WT/util"
 )
 
-func errorPrint(err error) {
-	if err != nil {
-		print(err.Error())
-		return
-	}
-}
+
 
 func main() {
 
 	deploy, err := util.ToDeploy()
-	errorPrint(err)
+	util.Errors(err)
 
 	week := util.ObtainWorkDayToCurrentWeek()
 
 	//文件读取
 	acquisition, err := FileAcquisition(deploy.FilePath)
-	errorPrint(err)
+	util.Errors(err)
 	// 文本提取
 	parsing, allContext := TextParsingRewrite(acquisition)
 	tableContent := entry.TableContent{}
 	tableContent = tableContent.SetValueInEntry(deploy, week, parsing, allContext)
 	util.ExcelRead(&tableContent,*deploy)
-	errorPrint(err)
+	util.Errors(err)
 	// 创建邮箱发送地址
 	// 写入发送内容模板
 	// 指定发送人
